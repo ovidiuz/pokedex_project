e773257355c46e89475198bee43e36a656fae77a
marose <marius.tudosoiu@stud.acs.upb.ro>
2023-01-10 23:09:39 +0200

    implement PokemonListAdapter

--- app/src/main/java/com/example/pokedexproject/adapters/PokemonListAdapter.kt
+++ app/src/main/java/com/example/pokedexproject/adapters/PokemonListAdapter.kt
@@ -1,23 +1,55 @@
 package com.example.pokedexproject.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedexproject.databinding.RowItemBinding
 import com.example.pokedexproject.models.Pokemon
 import timber.log.Timber
 
 private const val TAG = "PokemonListAdapter"
 
-class PokemonListAdapter : ListAdapter<Pokemon, ViewHolder>(PokemonDiffCallback()) {
+class PokemonListAdapter : ListAdapter<Pokemon, PokemonListAdapter.ViewHolder>(PokemonDiffCallback()) {
+    class ViewHolder private constructor(private val binding: RowItemBinding, private val navController: NavController) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(pokemon: Pokemon) {
+            Timber.d("$TAG: calling PokemonListAdapter.ViewHolder.bind() with pokemon ${pokemon.name}")
+
+            // Setup the views inside the RecyclerView row
+            binding.rowItemCardTitle.text = pokemon.name.capitalize()
+            binding.rowItemCardType.text = "Type: ${pokemon.type.capitalize()}"
+            binding.rowItemCardView.setOnClickListener {
+                // TODO: add navigation to the details page
+            }
+
+            // TODO: Setup Image for card view
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                Timber.d("$TAG: calling PokemonListAdapter.ViewHolder.from()")
+                val navController = parent.findFragment<Fragment>().findNavController()
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding, navController)
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         Timber.d("$TAG: onCreateViewHolder()")
-        TODO("Not yet implemented")
+        return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Timber.d("$TAG: onBindViewHolder()")
-        TODO("Not yet implemented")
+        val item = getItem(position)
+        holder.bind(item)
     }
 }
 
--- app/src/main/java/com/example/pokedexproject/util/Constants.kt
+++ app/src/main/java/com/example/pokedexproject/util/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "https://pokeapi.co/"
     const val TABLE_NAME = "pokemons"
     const val POKEMON_LIMIT = 100
+    const val BUNDLE_PROPERTY = "pokemon"
 }
