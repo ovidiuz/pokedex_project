2e4e19aa2b258f684db62d8401e4ed75eb9542ea
marose <marius.tudosoiu@stud.acs.upb.ro>
2023-01-10 22:55:43 +0200

    refactor PokemonListViewModel to leverage PokemonRepository

--- app/src/main/java/com/example/pokedexproject/viewmodels/PokemonListViewModel.kt
+++ app/src/main/java/com/example/pokedexproject/viewmodels/PokemonListViewModel.kt
@@ -1,12 +1,41 @@
 package com.example.pokedexproject.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.pokedexproject.models.Pokemon
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.pokedexproject.repository.PokemonRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.io.IOException
 
-class PokemonListViewModel : ViewModel() {
-    val _pokemonsList = MutableLiveData<List<Pokemon>>()
-    val pokemonList: LiveData<List<Pokemon>>
-        get() = _pokemonsList
+private const val TAG = "PokemonListviewModel"
+
+class PokemonListViewModel constructor(private val repository: PokemonRepository) : ViewModel() {
+    val pokemonList = repository.pokemons
+
+    init {
+        refreshDataFromRepository()
+    }
+
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch(Dispatchers.IO) {
+            Timber.d("$TAG: calling refreshDataFromRepository")
+            try {
+                repository.refreshPokemons()
+            } catch (networkError: IOException) {
+                Timber.e("$TAG: caught IOException error = $networkError")
+            }
+        }
+    }
+
+    class Factory(private val repository: PokemonRepository) : ViewModelProvider.Factory {
+        @Suppress("unchecked_cast")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(PokemonListViewModel::class.java)) {
+                return PokemonListViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
