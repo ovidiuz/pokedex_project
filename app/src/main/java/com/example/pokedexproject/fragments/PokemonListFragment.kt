b6543451ca5f6bee341222cf664404cd3ae8ddae
ovidiuz <ovidiu.zamfir98@gmail.com>
2023-01-10 20:53:24 +0200

    create list_fragment.xml file

--- app/src/main/java/com/example/pokedexproject/fragments/PokemonListFragment.kt
+++ app/src/main/java/com/example/pokedexproject/fragments/PokemonListFragment.kt
@@ -1,7 +1,8 @@
 package com.example.pokedexproject.fragments
 
 import androidx.fragment.app.Fragment
+import com.example.pokedexproject.R
 
-class PokemonListFragment : Fragment() {
+class PokemonListFragment : Fragment(R.layout.list_fragment) {
     //TODO("implement me")
 }
