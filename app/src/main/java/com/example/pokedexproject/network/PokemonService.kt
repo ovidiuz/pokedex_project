f9940204ef80298eb20068e3cdd36772f5c4cccc
Viorica Mancas <vioricamancas@yahoo.com>
2023-01-10 20:21:25 +0200

    add retrofit adapter for https://pokeapi.co

---
+++ app/src/main/java/com/example/pokedexproject/network/PokemonService.kt
@@ -0,0 +1,25 @@
+package com.example.pokedexproject.network
+
+import com.example.pokedexproject.util.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PokemonApi {
+    @GET("/api/v2/pokemon")
+    suspend fun getPokemonsList(@Query("limit") limit: Int): PokemonListDto
+
+    @GET("/api/v2/pokemon/{id}")
+    suspend fun getPokemonDetail(@Path("id") id: String): PokemonDto
+}
+
+object PokemonApiService {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    val retrofitService: PokemonApi = retrofit.create(PokemonApi::class.java)
+}
