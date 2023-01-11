91eb0ef4403222e4213c106df6298bca48e1c114
ovidiuz <ovidiu.zamfir98@gmail.com>
2023-01-11 16:49:18 +0200

    add the pokemon detail fragment to navigation

---
+++ app/src/main/java/com/example/pokedexproject/fragments/PokemonDetailFragment.kt
@@ -0,0 +1,8 @@
+package com.example.pokedexproject.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.pokedexproject.R
+
+class PokemonDetailFragment : Fragment(R.layout.detail_fragment) {
+    // TODO("implement me")
+}
