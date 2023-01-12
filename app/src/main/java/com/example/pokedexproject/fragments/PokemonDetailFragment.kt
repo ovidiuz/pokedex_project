4acca304ae0b57dc42d6d51a31d0804d44f728f4
ovidiuz <ovidiu.zamfir98@gmail.com>
2023-01-12 10:12:09 +0200

    implement the onViewCreated() method override for PokemonDetailFragment

--- app/src/main/java/com/example/pokedexproject/fragments/PokemonDetailFragment.kt
+++ app/src/main/java/com/example/pokedexproject/fragments/PokemonDetailFragment.kt
@@ -1,8 +1,45 @@
 package com.example.pokedexproject.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.pokedexproject.R
+import com.example.pokedexproject.databinding.DetailFragmentBinding
+import com.example.pokedexproject.models.Pokemon
+import com.example.pokedexproject.util.Constants
+import com.example.pokedexproject.viewmodels.PokemonDetailViewModel
+import timber.log.Timber
+
+private const val TAG = "PokemonDetailFragment"
 
 class PokemonDetailFragment : Fragment(R.layout.detail_fragment) {
-    // TODO("implement me")
+    private lateinit var binding: DetailFragmentBinding
+    private var viewModel: PokemonDetailViewModel? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Timber.d("$TAG: onViewCreated")
+        binding = DetailFragmentBinding.bind(view)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        requireArguments().let {
+            it.getParcelable<Pokemon>(Constants.BUNDLE_PROPERTY).let { pokemon ->
+                pokemon?.let {
+                    Timber.i("$TAG: Creating PokemonDetailViewModel with the following $pokemon object")
+                    val viewModelFactory = PokemonDetailViewModel.Factory(pokemon)
+                    viewModel = viewModelFactory.create(PokemonDetailViewModel::class.java)
+                }
+            }
+        }
+
+        viewModel?.pokemonProperty?.observe(viewLifecycleOwner) { pokemon ->
+            Timber.d("$TAG: Refreshing the existing views")
+            refreshViews(pokemon)
+        }
+    }
+
+    private fun refreshViews(pokemon: Pokemon?) {
+        TODO("Not yet implemented")
+    }
 }
